4d38cc9698c195bca188d88443d242dcd650cdee
Yongjun <wangyj641@gmail.com>
2024-01-03 09:04:30 +0800

    update

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 const session = require('koa-session')
 const static = require('koa-static')
 const views = require('koa-views')
-const router = require('koa-router')()
 
 const app = new Koa()
+const router = Router()
+
+const world_html =
+  '<div><a href="/">Index</a>&nbsp;&nbsp;&nbsp;<a href="/about">About</a>&nbsp;&nbsp;&nbsp;<a href="/world">World</a></div><h1>World</h1><script>var httpRequest = new XMLHttpRequest();//第一步：创建需要的对象\n' +
+  'httpRequest.open(\'POST\', \'postdata\', true); //第二步：打开连接\n' +
+  'httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");//设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）\n' +
+  'httpRequest.send(\'name=teswe&ee=ef\');//发送请求 将情头体写在send中\n' +
+  '/**\n' +
+  ' * 获取数据后的处理程序\n' +
+  ' */\n' +
+  'httpRequest.onreadystatechange = function () {//请求后的回调接口，可将请求成功后要执行的程序写在其中\n' +
+  '    if (httpRequest.readyState == 4 && httpRequest.status == 200) {//验证请求是否发送成功\n' +
+  '        var json = httpRequest.responseText;//获取到服务端返回的数据\n' +
+  '        console.log(json);\n' +
+  '    }\n' +
+  '};</script>'
 
 // ctx.render need it
 app.use(views(__dirname + '/views', {
@@ -39,6 +55,7 @@ const CONFIG = {
 app.use(session(CONFIG, app))
 
 app.use(async (ctx, next) => {
+  console.log('---------------- go to session')
   // ignore favicon
   if (ctx.path === '/favicon.ico') return;
   let n = ctx.session.views || 0;
@@ -63,21 +80,6 @@ const about = async (ctx, next) => {
   console.log('---------------- go to about')
   ctx.body = '<div><a href="/">Index</a>&nbsp;&nbsp;&nbsp;<a href="/about">About</a>&nbsp;&nbsp;&nbsp;<a href="/world">World</a></div><h1>About</h1>'
 }
-
-const world_html =
-  '<div><a href="/">Index</a>&nbsp;&nbsp;&nbsp;<a href="/about">About</a>&nbsp;&nbsp;&nbsp;<a href="/world">World</a></div><h1>World</h1><script>var httpRequest = new XMLHttpRequest();//第一步：创建需要的对象\n' +
-  'httpRequest.open(\'POST\', \'postdata\', true); //第二步：打开连接\n' +
-  'httpRequest.setRequestHeader("Content-type","application/x-www-form-urlencoded");//设置请求头 注：post方式必须设置请求头（在建立连接后设置请求头）\n' +
-  'httpRequest.send(\'name=teswe&ee=ef\');//发送请求 将情头体写在send中\n' +
-  '/**\n' +
-  ' * 获取数据后的处理程序\n' +
-  ' */\n' +
-  'httpRequest.onreadystatechange = function () {//请求后的回调接口，可将请求成功后要执行的程序写在其中\n' +
-  '    if (httpRequest.readyState == 4 && httpRequest.status == 200) {//验证请求是否发送成功\n' +
-  '        var json = httpRequest.responseText;//获取到服务端返回的数据\n' +
-  '        console.log(json);\n' +
-  '    }\n' +
-  '};</script>'
 
 const world = async (ctx, next) => {
   console.log('---------------- go to world')
